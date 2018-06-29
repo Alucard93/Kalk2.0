1b4710315a287a55b1d5683072359dbcdc20bd04
BroHPotato <giuseppevitobitetti@libero.it>
2018-06-29 16:53:54 +0200

    update cie

--- Kalk/cie_xyz.cpp
+++ Kalk/cie_xyz.cpp
@@ -1,48 +1,92 @@
-#include "cie_xyz.h"
+#include <cie_xyz.h>
+
 /**
- * base constructor for CIExyz colour rappresentation
+ * Constructor for CIE xyz colour rappresentation from Colour pointer
  * @brief CIExyz::CIExyz
+ * @param c
+ */
+
+CIExyz::CIExyz(const Colour* c){
+    CIExyz* b = nullptr;
+    b=static_cast<CIExyz*>(c->getCIE());
+    if(b==nullptr)
+        throw new IllegalColourException("illegal colour definition");
+    x=b->x;
+    y=b->y;
+    z=b->z;
+}
+/**
+ * Constructor for CIE xyz colour rappresentation from double precision numbers
+ * @brief CIExyz::CIExyz
+ * @param _x
+ * @param _y
  */
 CIExyz::CIExyz(double _x,double _y, double _z){
+    if(_x<lower_limit || _x>upper_limit || _y<lower_limit || _y>upper_limit)
+        throw new IllegalColourException("value out of boundires");
     x=_x;
     y=_y;
-    z=_z;
+    if(qFuzzyCompare(_z,(1-x-y)))
+        z=_z;
+    else
+        z=1-x-y;
 }
 
-/**
- * base desctructor method
- * @brief CIExyz::~CIExyz
- */
 CIExyz::~CIExyz(){
+    delete static_cast<Colour*>(this);
 }
 
-
-/*void CIExyz::show_rap(){
+void CIExyz::show_rap()const{
     std::cout<<"CIE xyz "<< x << " "<<y<<" "<<z;
 }
 
 Colour* CIExyz::negate() const{
-    float nx=1-x;
-    float ny=1-y;
+    double nx=1-x;
+    double ny=1-y;
     return  new CIExyz(nx,ny);
 }
 
-Colour* CIExyz::mix(Colour* c)const{
-    CIExyz* b= nullptr_t();
-    if(dynamic_cast<CIExyz*>(c))
-        b=static_cast<CIExyz*>(c);
-    else
+Colour* CIExyz::mix(const Colour* c)const{
+    CIExyz* b=nullptr;
+    b=static_cast<CIExyz*>(c->getCIE());
+    if(b==nullptr)
         throw new IllegalColourException("illegal object");
-    float nx= (b->x+this->x)/2;
-    float ny= (b->y+this->y)/2;
+    double nx= (b->x+this->x)/2;
+    double ny= (b->y+this->y)/2;
     return new CIExyz(nx,ny);
 
 }
-*/
-CIExyz* CIExyz::getCIE() const{
+
+Colour* CIExyz::getCIE() const{
     return new CIExyz(x,y);
 }
-void CIExyz::getciexyz(){
-    cout<<x<<'\n'<<y<<'\n'<<z<<'\n';
+
+double CIExyz::get_component(int c) const{
+    switch (c) {
+    case 0: {double _x =x;
+            return _x;}
+    case 1: {double _y =y;
+            return _y;}
+    case 2: {double _z =z;
+            return _z;}
+    }
+    return 0;
 }
 
+double CIExyz::get_x() const{
+    double _x = x;
+    return  _x;
+}
+
+double CIExyz::get_y() const{
+    double _y = y;
+    return  _y;
+}
+
+double CIExyz::get_z() const{
+    double _z = z;
+    return  _z;
+}
+
+int CIExyz::lower_limit=0;
+int CIExyz::upper_limit=1;
