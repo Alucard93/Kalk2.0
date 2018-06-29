2e483d91d63ed7df9f1e96df0afec0fab8c63ce9
BroHPotato <giuseppevitobitetti@libero.it>
2018-06-29 16:54:27 +0200

    update cie

--- Kalk/cie_xyz.h
+++ Kalk/cie_xyz.h
@@ -1,23 +1,29 @@
 #ifndef CIE_XYZ_H
 #define CIE_XYZ_H
-#include "colour.h"
-#include <iostream>
+#include <colour.h>
 
 class CIExyz : public Colour{
 public:
+    CIExyz(const Colour* c);
     CIExyz(double _x=0, double _y=0, double _z=0);
     ~CIExyz();
-    //void show_rap();
-    //void show_desc();
-    //Colour* convert(Colour* from);
-    //Colour* negate() const;
-    //Colour* mix(Colour* c)const;
-    CIExyz* getCIE() const;
-    void getciexyz();
+    void show_rap() const;
+    Colour* negate() const;
+    Colour* mix(const Colour* c)const;
+    Colour* getCIE() const;
+
+protected:
+    double get_component(int c) const;
+    double get_x() const;
+    double get_y() const;
+    double get_z() const;
+
 private:
   double x;
   double y;
   double z;
+  static int upper_limit;
+  static int lower_limit;
 };
 
 #endif // CIE_XYZ_H
