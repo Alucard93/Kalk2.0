c3c769884e3e716e7236782eef424bfe9fe96854
BroHPotato <giuseppevitobitetti@libero.it>
2018-06-29 16:55:22 +0200

    update colour

--- Kalk/colour.h
+++ Kalk/colour.h
@@ -1,19 +1,16 @@
 #ifndef COLOUR_H
 #define COLOUR_H
 #include <iostream>
-#include "illegalcolourexception.h"
+#include <illegalcolourexception.h>
+#include <QtGlobal>
 using namespace std;
 class Colour{
 public:
-  //virtual ~Colour();
-  //virtual void show_rap()=0;
-  //virtual void show_desc()=0;
-  //virtual Colour* convert(Colour* from)=0;
-  //virtual Colour* negate()const =0;
-  //virtual Colour* mix(Colour* c1)const =0;
-
-private:
-  //string desc;
+    virtual ~Colour();
+    virtual void show_rap()const =0;
+    virtual Colour* getCIE()const =0;
+    virtual Colour* negate()const =0;
+    virtual Colour* mix(const Colour* c1)const =0;
 };
 
 #endif // COLOUR_H
