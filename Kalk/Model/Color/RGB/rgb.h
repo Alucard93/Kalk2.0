5f9ac3aa0cfac160b2334c3e9e7d2884f8dbfea8
Jatus <gianmarco.pettinato@gmail.com>
2018-08-20 16:57:44 +0200

    Works switching between CIExyz and RGB

--- Kalk/Model/Color/RGB/rgb.h
+++ Kalk/Model/Color/RGB/rgb.h
@@ -6,11 +6,15 @@ public:
     RGB(int r=0, int g=0, int b=0);
     RGB(const Color* c);
     RGB(const RGB* c);
+
     int getNumberOfComponets() const;
     void setComponents(QVector<double> componets);
+
     QString getRappresentation()const;
     Color* negate() const;
     Color* mix(const Color* c) const;
+    QVector<QString> availableOperations() const;
+
     //Color* getCIE() const;
     CIExyz* getCIE(int t_r, int t_g, int t_b) const;
     Color* getColorFromVector(QVector<double> comp) const;
@@ -21,12 +25,11 @@ private:
     int sRGB[3];
     static double CIE_RGB[3][3]; //contains matrix to transforma CIE color rappresentation to sRGB
     static double RGB_CIE[3][3];
-    static int lower_limit;
-    static int upper_limit;
-    static int componets;
+    constexpr static const int lower_limit = 0;
+    constexpr static const int upper_limit = 255;
+    constexpr static const int componets = 3;
     QVector<double> rgb2CieXyz(QVector<double> components) const;
+    static const QVector<QString> implementedMethods;
 };
-
-Q_DECLARE_METATYPE(RGB)
 
 #endif // RGB_H
